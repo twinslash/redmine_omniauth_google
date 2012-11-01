febcb9c939028a8f7e7f122c051db9f55842e45c
Дмитрий Коваленок <dmitry.kovalenok@twinslash.com>
2012-11-01 14:13:15 +0300

    updating Gemfile.lock

--- Gemfile
+++ Gemfile
@@ -1,6 +1,2 @@
 gem 'oauth2'
 gem 'json'
-
-group :development, :test do
-  gem 'pry'
-end
--- test/functional/redmine_oauth_controller_test.rb
+++ test/functional/redmine_oauth_controller_test.rb
@@ -11,18 +11,26 @@ class RedmineOauthControllerTest < ActionController::TestCase
     OAuth2::Response.any_instance.stubs(body: body.to_json)
   end
 
-  def test_login_with_omniauth_for_new_user
-    Setting.self_registration = '3'
+  def new_user options = nil
     user_credentials = {:firstname => 'Cool',
                         :lastname => 'User',
-                        :mail => 'user@somedomain.com'}
-    User.where(user_credentials).delete_all
-    new_user = User.new(user_credentials)
-    new_user.login = 'cool_user'
-    
+                        :mail => 'user@somedomain.com'}.merge(options)
+    user = User.new(user_credentials)
+    user.login = options[:login] || 'cool_user'
+    user
+  end
+
+  def test_login_with_omniauth_for_new_user
+    Setting.self_registration = '3'
+    user
     set_response_body_stub({verified_email: "true", name: [new_user.firstname, new_user.lastname]*" ", given_name: new_user.firstname, family_name: new_user.lastname, email: new_user.mail})
     get :oauth_google_callback, :email => new_user.mail
     assert_redirected_to controller: 'my', action: 'account'
+  end
+
+  def test_login_with_invalid_oauth_provider
+    Setting.self_registration = '0'
+
   end
 
   #assert existing_user.save!
